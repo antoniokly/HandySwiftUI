ea0df1aad67b36b093e20036d2fde08da8ff971f
Antonio Yip <antoniokly@users.noreply.github.com>
2020-05-27 17:30:44 +1200

    VGrid

--- Showcase/ContentView.swift
+++ Showcase/ContentView.swift
@@ -12,32 +12,87 @@ struct ContentView: View {
     @State var currentPage: Int = 0
     
     var body: some View {
-        PageView(pageCount: 5, currentPage: $currentPage, isNavigating: nil) { index in
-            if index == 0 {
-                Color.gray
-                    .frame(maxHeight: .infinity)
-                    .edgesIgnoringSafeArea(.all)
-            } else if index == 1 {
-                Text("Page 2")
-            } else if index == 2 {
-                Text("Page 3")
-            } else {
-                ZStack {
-                    Color.green
-                    Text("Page 4")
-                }
-            }
-        }.indicator(currentPageSize: 10,
+        PageView(pageCount: 4,
+                 currentPage: $currentPage,
+                 isNavigating: nil,
+                 content: page
+        ).indicator(currentPageSize: 10,
                     defaultSize: 5,
                     currentPageColor: .accentColor,
                     defaultColor: .orange,
                     opacity: 0.8
         )
     }
+    
+    @ViewBuilder
+    func page(_ index: Int) -> some View {
+        if index == 0 {
+            Color.gray
+                .frame(maxHeight: .infinity)
+                .edgesIgnoringSafeArea(.all)
+        } else if index == 1 {
+            VStack {
+                HGrid(spacing: 2,
+                      ratios: [1,1,1,1],
+                      content: cell)
+                
+                HGrid(spacing: 2,
+                      ratios: [1,3],
+                      content: cell)
+            }.padding()
+        } else if index == 2 {
+            HStack {
+                VGrid(spacing: 5,
+                      ratios: [1,1,1,1],
+                      content: cell)
+                VGrid(spacing: 5,
+                      ratios: [1,2,1],
+                      content: cell)
+            }.padding()
+        } else {
+            VStack {
+                Toggle(isOn: .constant(false)) {
+                    Text("CustomToggleStyle")
+                    
+                }.toggleStyle(CustomToggleStyle())
+                
+                Divider()
+                                    
+                Toggle(isOn: .constant(false)) {
+                    Text("CustomToggleStyle").font(.largeTitle)
+                }.toggleStyle(
+                    CustomToggleStyle(width: 150,
+                                      height: 60,
+                                      gap: 3,
+                                      onText: "開",
+                                      offText: "關",
+                                      statusTextColor: .white,
+                                      onColor: .green,
+                                      offColor: .red,
+                                      buttonColor: .white))
+            }.padding()
+        }
+    }
+    
+    @ViewBuilder
+    func cell(_ index: Int) -> some View {
+        if index % 2 == 0 {
+            RoundedRectangle(cornerRadius: 40)
+                .foregroundColor(Color.red)
+                .opacity(0.5)
+        } else {
+            Rectangle().iOS { $0
+                .foregroundColor(.blue)
+            }.watchOS { $0
+                .foregroundColor(.green)
+                .opacity(0.5)
+            }
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(currentPage: 2)
     }
 }
